810b93004e03c6754088e8573e55a768aba9c2ef
damioke.info@gmail.com <damioke.info@gmail.com>
2019-05-23 11:43:22 +0200

    Program file to execute

---
+++ job.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export PATH=$PATH:/C/wamp/bin/mysql/mysql5.6.17/bin/main.py
+export PATH=$PATH:/C/wamp/bin/mysql/mysql5.6.17/bin/variables.py
+export PATH=$PATH:/C/wamp/bin/mysql/mysql5.6.17/bin/sql_queries.py
+
+python main.py
