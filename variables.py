39a5d81686cc3984c8bcaa8272d1e2a837963a5e
damioke.info@gmail.com <damioke.info@gmail.com>
2019-05-23 11:49:08 +0200

    Provide necessary credentials

---
+++ variables.py
@@ -0,0 +1,19 @@
+# dataware house name: dw should be existing in MySQL
+datawarehouse_name = 'seattle_crime'
+useforce_tbl = 'useforce'
+crime_tbl = 'crimes'
+
+# provide login credentials to MySQL
+host='localhost'
+user='root'
+password='esther'
+
+# names used to store output files 
+report_1 = "'report_1.csv'"
+report_2 = "'report_2.csv'"
+report_3 = "'report_3.csv'"
+report_4 = "'report_4.csv'"
+
+# names used to store input files 
+useforce_file = "'u_of_f_data.csv'"
+crimes_file = "'crime_data.csv'"
